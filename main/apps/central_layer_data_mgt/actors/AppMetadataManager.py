f5ace693eeff1f735a914910d0ba5511b1f7aa43
jiangtommy1012 <jiangtommy1012@gmail.com>
2023-11-09 04:52:40 +0000

    feat: add functions in AMM actor

--- main/apps/central_layer_data_mgt/actors/AppMetadataManager.py
+++ main/apps/central_layer_data_mgt/actors/AppMetadataManager.py
@@ -14,3 +14,36 @@ def save_application_metadata(request):
     except Exception as e:
         log_writer('ERROR', save_application_metadata, (request,), message=e)
         return HttpResponse("save_application_metadata error")
+
+@log_trigger("INFO")
+def add_application_metadata(request):
+    """
+    add the application metadata
+    """
+    try:
+        return HttpResponse("add_application_metadata finish")
+    except Exception as e:
+        log_writer('ERROR', add_application_metadata, (request,), message=e)
+        return HttpResponse("add_application_metadata error")
+
+@log_trigger("INFO")
+def delete_application_metadata(request):
+    """
+    delete the application metadata
+    """
+    try:
+        return HttpResponse("delete_application_metadata finish")
+    except Exception as e:
+        log_writer('ERROR', delete_application_metadata, (request,), message=e)
+        return HttpResponse("delete_application_metadata error")
+
+@log_trigger("INFO")
+def modify_application_metadata(request):
+    """
+    modify the application metadata
+    """
+    try:
+        return HttpResponse("modify_application_metadata finish")
+    except Exception as e:
+        log_writer('ERROR', modify_application_metadata, (request,), message=e)
+        return HttpResponse("modify_application_metadata error")
--- main/apps/central_layer_data_mgt/api/urls.py
+++ main/apps/central_layer_data_mgt/api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from main.apps.central_layer_data_mgt.actors import AppMetadataManager
 
 urlpatterns = [
-    path('central_layer_data_mgt/AppMetadataManager/save_application_metadata', AppMetadataManager.save_application_metadata)
+    path('central_layer_data_mgt/AppMetadataManager/save_application_metadata', AppMetadataManager.save_application_metadata),
+    path('central_layer_data_mgt/AppMetadataManager/add_application_metadata', AppMetadataManager.add_application_metadata),
+    path('central_layer_data_mgt/AppMetadataManager/delete_application_metadata', AppMetadataManager.delete_application_metadata),
+    path('central_layer_data_mgt/AppMetadataManager/modify_application_metadata', AppMetadataManager.modify_application_metadata)
 ]
