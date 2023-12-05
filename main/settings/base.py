adb463964fe8a5bf02741437ea9070803d2a1160
jiangtommy1012 <jiangtommy1012@gmail.com>
2023-12-05 13:04:12 +0000

    feat: db connect success

--- main/settings/base.py
+++ main/settings/base.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '192.168.183.128',
+        'POST': '5432'
     }
 }
 
